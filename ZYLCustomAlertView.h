1f0a02d908e9678b2cdc0f1c23e1551721ad86c5
zylcold <zylcold@163.com>
2016-04-25 10:54:25 +0800

    Update ZYLCustomAlertView.h

--- ZYLCustomAlertView.h
+++ ZYLCustomAlertView.h
@@ -7,26 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
-@protocol HQCustomInputView
+@protocol ZYLCustomInputView
 @required
 - (void)inputViewBecomeFirstResponder;
 - (void)inputViewResignFirstResponder;
 @end
 
-typedef NS_ENUM(NSInteger, HQShowAlertStyle) {
-    HQShowAlertFromBottom,
-    HQShowAlertFromCenter,
-    HQShowAlertFromCustom
+typedef NS_ENUM(NSInteger, ZYLShowAlertStyle) {
+    ZYLShowAlertFromBottom,
+    ZYLShowAlertFromCenter,
+    ZYLShowAlertFromCustom
 };
 @interface ZYLCustomAlertView : UIView
 /**&lt; 展示View 只需设置size  */
 @property(nonatomic, strong) UIView *contentView;
 /**&lt; 带有InputView */
-@property(nonatomic, strong) UIView<HQCustomInputView> *contentInputView;
+@property(nonatomic, strong) UIView<ZYLCustomInputView> *contentInputView;
 /**&lt; 背景 defult [UIColor red:0 green:0 blue:0 alpha:0.5]; */
 @property(nonatomic, strong) UIColor *alertBackgroundColor;
-/**&lt; 展示的位置 defult HQShowAlertFromBottom*/
-@property(nonatomic, assign) HQShowAlertStyle showStyle;
+/**&lt; 展示的位置 defult ZYLShowAlertFromBottom*/
+@property(nonatomic, assign) ZYLShowAlertStyle showStyle;
 /**&lt; 点击其他区域是否隐藏 defult Yes */
 @property(nonatomic, assign) BOOL entableTapDismiss;
 /**&lt; 隐藏时的操作 */
@@ -39,13 +39,13 @@ typedef NS_ENUM(NSInteger, HQShowAlertStyle) {
 
 - (instancetype)initWithContentView:(UIView *)contentView;
 
-- (instancetype)initWithContentInputView:(UIView<HQCustomInputView> *)contentView;
+- (instancetype)initWithContentInputView:(UIView<ZYLCustomInputView> *)contentView;
 
 /**&lt; 使用默认方式展示一个自定义View */
-+ (instancetype)addCustomView:(UIView *)view forPosition:(HQShowAlertStyle)position;
++ (instancetype)addCustomView:(UIView *)view forPosition:(ZYLShowAlertStyle)position;
 
 /**&lt; 使用默认方式展示一个自定义View */
-+ (instancetype)addCustomView:(UIView *)view forPosition:(HQShowAlertStyle)position animaton:(BOOL)animaton;
++ (instancetype)addCustomView:(UIView *)view forPosition:(ZYLShowAlertStyle)position animaton:(BOOL)animaton;
 
 /**&lt; 展示 */
 - (void)show;
